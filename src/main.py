4aa5d538fd409a0c048676a42cb6f26454719767
John Cameron Thomas <johncameronthomas@icloud.com>
2023-03-09 12:34:07 -0500

    basic outline code and html

--- src/main.py
+++ src/main.py
@@ -1,12 +1,28 @@
 import web
 
+render = web.template.render('templates/', base='layout')
 urls = (
-    '/', 'index'
-)
+    '/', 'index',
+    '/about', 'about',
+    '/posts', 'posts',
+    '/contact', 'contact'
+    )
 
 class index:
     def GET(self):
-        return 'Hello world!'
+        return render.index()
+    
+class about:
+    def GET(self):
+        return render.about()
+    
+class posts:
+    def GET(self):
+        return render.posts()
+    
+class contact:
+    def GET(self):
+        return render.contact()
 
 if __name__ == '__main__':
     app = web.application(urls, globals())
