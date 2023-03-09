c7b3e8ba598372b503ccf27aa5bc0492fb2ebaa9
John Cameron Thomas <johncameronthomas@icloud.com>
2023-03-09 13:15:31 -0500

    added posts functionality and more html

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 import web
+import json
+import os
 
 render = web.template.render('templates/', base='layout')
 urls = (
     '/', 'index',
+    '/home', 'home',
     '/about', 'about',
     '/posts', 'posts',
     '/contact', 'contact'
@@ -10,7 +13,11 @@ urls = (
 
 class index:
     def GET(self):
-        return render.index()
+        raise web.seeother('/home')
+    
+class home:
+    def GET(self):
+        return render.home()
     
 class about:
     def GET(self):
@@ -18,7 +25,17 @@ class about:
     
 class posts:
     def GET(self):
-        return render.posts()
+        posts = []
+        filenames = os.listdir(os.getcwd() + '/posts')
+        if len(filenames) == 0:
+            return render.error('There are no posts to display.')
+        else:
+            for filename in filenames:
+                f = open('posts/' + filename, 'r')
+                posts.append(json.load(f))
+                f.close()
+            posts = tuple(posts)
+            return render.posts(posts)
     
 class contact:
     def GET(self):
